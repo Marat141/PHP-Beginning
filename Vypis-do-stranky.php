3f8a99b72d992efb44433c960752526a5381a766
Marat141 <terest141@gmail.com>
2025-02-10 14:37:45 +0100

    Vypis-do-stranky

---
+++ Vypis-do-stranky.php
@@ -0,0 +1,22 @@
+<?php
+
+    // Výpis do stránky
+    # Výpis do stránky
+    /* 
+        Komentář 
+        na 
+        více
+        řádku
+    */
+    echo "Marat Fedorov";
+    echo "<br>";
+    echo 100;
+    echo "<br>";
+    echo "Plzeň";
+    echo "<br>";
+    
+    var_dump("Marat Fedorov");
+    echo "<br>";
+    var_dump(100);
+    echo "<br>";
+    var_dump(10.4);
