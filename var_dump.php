77e04f151b15f83327b4186a5b1f6196cef7bd5b
Marat141 <terest141@gmail.com>
2025-03-03 09:18:08 +0100

    var_dump

---
+++ var_dump.php
@@ -0,0 +1,6 @@
+<?php
+
+$array = ["Marat", "Gabriel", "Tomáš"];
+
+var_dump($array); //var_dump() funkce, která vypíše podrobné informace o proměnné, včetně jejího typu a hodnoty. Používá se hlavně pro ladění kódu
+?>
